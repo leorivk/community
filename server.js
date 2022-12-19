51b840fbd5c24dbfd7d1f9b6a5be78c967b2a244
leorivk <leorivk@gmail.com>
2022-12-19 23:15:19 +0900

    Configure template engine pug

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express();
 
 const PORT = 4000;
 
+const rootRouter = express.Router();
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + '/views');
+
+app.use("/", rootRouter);
+
+const home = (req, res) => {
+    return res.render("home");
+}
+
+rootRouter.get("/", home);
+
 const handleListening = () => {
     console.log(`Listening from http://localhost:${PORT}/ ✅`);
 }
